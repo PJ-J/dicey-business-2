267e1ef27265ef590bf89a2f6d30b217833b999f
PJ Jones <pj.jones4000.com>
2022-03-08 14:35:35 -0600

    first commit, redid everything except advanced stuff

---
+++ app.js
@@ -0,0 +1,42 @@
+let diceArr = [];
+let dieCounter = 0;
+let sum = 0;
+let diceHolder = document.getElementById("diceHolder");
+
+class Die {
+  constructor() {
+    let dieDiv = document.createElement("div");
+    dieDiv.className = "die";
+    dieDiv.id = dieCounter;
+    this.roll();
+    dieDiv.textContent = this.value;
+    diceHolder.appendChild(dieDiv);
+    dieCounter++;
+  }
+
+  roll() {
+    this.value = Math.floor(Math.random() * 6) + 1;
+  }
+}
+
+let addDie = document.getElementById("addDie");
+addDie.addEventListener("click", () => {
+  let d1 = new Die();
+  diceArr.push(d1);
+});
+
+let rollDice = document.getElementById("rollDice");
+rollDice.addEventListener("click", () => {
+  for (let i = 0; i < diceArr.length; i++) {
+    diceArr[i].roll();
+    document.getElementById(i).textContent = diceArr[i].value;
+  }
+});
+
+document.getElementById("sumDice").addEventListener("click", function () {
+  diceArr.forEach((die) => {
+    sum += die.value;
+  });
+  alert(`the sum is ${sum}.`);
+  sum = 0;
+});
